190f75358ac4165f1f46f1901c76a2fc1fa4763b
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-11-07 20:08:35 +0600

    Lesson commit

---
+++ BaseElementsLesson/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BaseElementsLesson
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //searchBtn.Click += tab najat` 1 variant
+            //searchBtn.Click += (sender, args) => { }; 2 variant
+
+        }
+
+        private void SearchButtonClick(object sender, RoutedEventArgs e)
+        {
+            browser.Navigate($"https://www.{searchTB.Text}");
+        }
+    }
+}
